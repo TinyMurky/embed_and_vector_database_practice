b959dcb655f40f6bb8c18e5e8cd1ec434f039f5a
TinyMurky <murky0830@gmail.com>
2024-06-29 14:37:49 +0800

    lyric

--- src/app/main.py
+++ src/app/main.py
@@ -4,7 +4,7 @@ from app.qdrant.qdrant import QdrantSingleton
 
 def main():
     embedding_model = EmbeddingModel()
-    sentences = ["Hello, World!", "I am Kyaru!", "I am a virtual YouTuber!"]
+    sentences = ["Hello, World!", "I am Bob!", "I am a Sailer!"]
 
     embeddings = embedding_model.encode(sentences)
     print("Dimension: ", len(embeddings[0]))  # 384 個向量
@@ -13,22 +13,23 @@ def main():
     print("type of embedding: ", type(embedding), type(embedding[0]))
 
     # qdrant
-    american_idiots = [
-        {"id": "1", "lyric": "Don't wanna be an American idiot"},
-        {"id": "2", "lyric": "Don't want a nation under the new media"},
-        {"id": "3", "lyric": "And can you hear the sound of hysteria?"},
-        {"id": "4", "lyric": "The subliminal mindfuck America"},
+    drunken_sailer = [
+        {"id": "1", "lyric": "What will we do with a drunken sailor"},
+        {"id": "2", "lyric": "Early in the morning"},
+        {"id": "3", "lyric": "Way hay and up she rises"},
+        {"id": "4", "lyric": "have his belly with a rusty razor"},
+        {"id": "5", "lyric": "Put him in a long boat till his sober"},
     ]
 
     qdrant = QdrantSingleton()
     collection_name = "Lyrics"
     qdrant.recreate_collection(collection_name)
 
-    embedding_array = [qdrant.get_embedding(text["lyric"]) for text in american_idiots]
+    embedding_array = [qdrant.get_embedding(text["lyric"]) for text in drunken_sailer]
 
-    qdrant.upsert_vectors(embedding_array, collection_name, american_idiots)
+    qdrant.upsert_vectors(embedding_array, collection_name, drunken_sailer)
 
-    query_text = "stupid american"
+    query_text = "Is drunken sailor a good song?"
     search_result = qdrant.search_for_qdrant(query_text, collection_name, limit_k=1)
 
     print(f"尋找: {query_text}", search_result)
